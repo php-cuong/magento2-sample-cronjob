a73299eb7ea66dd560496904345ed8bda795edc0
gg_cuongnq <cuongnq@greenglobal.vn>
2017-01-15 03:22:36 +0700

    add module SampleSystemConfiguration

---
+++ PHPCuong/SampleSystemConfiguration/registration.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ *
+ * @Author              Ngo Quang Cuong <bestearnmoney87@gmail.com>
+ * @Date                2017-01-14 08:55:09
+ * @Last modified by:   nquangcuong
+ * @Last Modified time: 2017-01-14 08:55:31
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'PHPCuong_SampleSystemConfiguration',
+    __DIR__
+);
